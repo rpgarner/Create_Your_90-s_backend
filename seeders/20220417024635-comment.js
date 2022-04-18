7a3bb3bcf167df7e109d04d118e5c18983460a76
Ryan Garner <rpgarner22@gmail.com>
2022-04-18 08:05:48 -0600

    all seed info is ready

--- seeders/20220417024635-comment.js
+++ seeders/20220417024635-comment.js
@@ -1,24 +1,41 @@
 "use strict";
 
 module.exports = {
-  async up(queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-     */
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert("comments", 
+    [
+      {
+        description: 'Miss thje good ole,days or playing pokemon on Game boy',
+        likes: 2,
+        name: 'Jenna',
+        userId: 5,
+        postId: 5,
+      },
+      {
+        description: `N'SYNC is everything, way better then backstreet boys` ,
+        likes: 22,
+        name: 'Angie',
+        userId: 2,
+        postId: 18,
+      },
+      {
+        description: 'I still watch friends, this show never gets old',
+        likes: 7,
+        name: 'Koa',
+        userId: 1,
+        postId: 19,
+      },
+      {
+        description: 'Still lost on the oregon trail, could never beat this game so hard. ',
+        likes: 1,
+        name: 'Ryan',
+        userId: 4,
+        postId: 21,
+      },
+    ]);
   },
 
-  async down(queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+  down: async (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete("comments");
   },
 };
