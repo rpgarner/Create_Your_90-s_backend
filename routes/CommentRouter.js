d1a66509f1868b24beffc5978a46fff07c55880d
Jenna Allgeier <jenna.allgeier@gmail.com>
2022-04-19 10:56:19 -0400

    protected comment routes

--- routes/CommentRouter.js
+++ routes/CommentRouter.js
@@ -1,10 +1,20 @@
 const Router = require("express").Router();
 const controller = require("../controllers/CommentController");
+const middleware = require('../middleware')
 
 Router.get("/recent", controller.getAllComments);
 Router.get("/:post_id", controller.GetCommentDetails);
-Router.post("/:user_id/:post_id", controller.newComment);
-Router.put("/:comment_id", controller.UpdateComment);
-Router.delete("/:comment_id", controller.DeleteComment);
+Router.post("/:user_id/:post_id", 
+    middleware.stripToken,
+    middleware.verifyToken,
+    controller.newComment);
+Router.put("/:comment_id", 
+    middleware.stripToken,
+    middleware.verifyToken,
+    controller.UpdateComment);
+Router.delete("/:comment_id", 
+    middleware.stripToken,
+    middleware.verifyToken,
+    controller.DeleteComment);
 
 module.exports = Router;
