55896899e39321811dfe8f38d143f6a8284c1820
Jenna Allgeier <jenna.allgeier@gmail.com>
2022-04-19 10:27:04 -0400

    protected post routes

--- routes/PostRouter.js
+++ routes/PostRouter.js
@@ -1,9 +1,14 @@
 const Router = require("express").Router();
 const controllers = require("../controllers/PostController");
+const middleware = require('../middleware')
 
 Router.get("/recent", controllers.GetRecentPosts);
 Router.get("/:postId", controllers.GetPostDetails);
-Router.post("/:user_id", controllers.CreatePost);
+Router.post(
+    "/:user_id",
+    middleware.stripToken,
+    middleware.verifyToken, 
+    controllers.CreatePost);
 Router.put("/:post_id", controllers.UpdatePost);
 Router.delete("/:post_id", controllers.DeletePost);
 
