aa6b808a748f150b0b6845d733735702fe31f886
Jenna Allgeier <jenna.allgeier@gmail.com>
2022-04-20 11:14:51 -0400

    added comment controller

--- routes/CommentRouter.js
+++ routes/CommentRouter.js
@@ -3,7 +3,7 @@ const controller = require("../controllers/CommentController");
 const middleware = require('../middleware')
 
 Router.get("/recent", controller.getAllComments);
-Router.get("/:post_id", controller.GetCommentDetails);
+Router.get("/:post_id", controller.GetAllCommentsByPostPk);
 Router.post("/:user_id/:post_id", 
     middleware.stripToken,
     middleware.verifyToken,
